c99b6f722d71d41429384fe70e588cb925a17634
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-06 00:11:17 -0600

    Update mphvpnspeedtest.sh

--- mphvpnspeedtest.sh
+++ mphvpnspeedtest.sh
@@ -1,252 +1,251 @@
-windscribe connect    Bukhansan | tee -a output.txt
+windscribe connect    Bukhansan | tee -a mphvpnspeed.txt
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Hangang  | tee -a output.txt
+windscribe connect    Hangang  | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Metro | tee -a output.txt
+windscribe connect    Metro | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Victoria | tee -a output.txt
+windscribe connect    Victoria | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Ataturk | tee -a output.txt
+windscribe connect    Ataturk | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Crumpets | tee -a output.txt
+windscribe connect    Crumpets | tee -a mphvpnspeed.txt
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Custard | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
 
-windscribe connect    Alphorn | tee -a output.txt
-
+windscribe connect    Custard | tee -a mphvpnspeed.txt
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    "No Vampires" | tee -a output.txt
-
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    Fjord     | tee -a output.txt
-
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    Canal | tee -a output.txt
-
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    Seine | tee -a output.txt
-
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    Castle | tee -a output.txt
-
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    "Expo 67" | tee -a output.txt
-
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-windscribe connect    "Old Port" | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
 
-windscribe connect    "Comfort Zone" | tee -a output.txt
+windscribe connect    Alphorn | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    "The 6" | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Granville | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Vansterdam            | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Mountain | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Ranch | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Cub | tee -a output.txt
+windscribe connect    "No Vampires" | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Snow | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Vice | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Empire | tee -a output.txt
-sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
-  sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
-sleep 10
-
-windscribe connect    Precedent | tee -a output.txt
+windscribe connect    Fjord     | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Dogg | tee -a output.txt
+windscribe connect    Canal | tee -a mphvpnspeed.txt
 
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
-windscribe connect    Cobain | tee -a output.txt
+windscribe connect    Seine | tee -a mphvpnspeed.txt
+
 sleep 10
-  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
   sleep 10
-  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    Castle | tee -a mphvpnspeed.txt
+
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    "Expo 67" | tee -a mphvpnspeed.txt
+
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    "Old Port" | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
 sleep 10
 
+windscribe connect    "Comfort Zone" | tee -a mphvpnspeed.txt
+
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    "The 6" | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Granville | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Vansterdam            | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Mountain | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Ranch | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Cub | tee -a mphvpnspeed.txt
+
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    Snow | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Vice | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Empire | tee -a mphpingtest.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+
+windscribe connect    Precedent | tee -a mphvpnspeed.txt
+
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    Dogg | tee -a mphvpnspeed.txt
+
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
+windscribe connect    Cobain | tee -a mphvpnspeed.txt
+sleep 10
+  ping        us-east.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping    europe.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+  sleep 10
+  ping         asia.ethash-hub.miningpoolhub.com:20535 -c 15 | tee -a mphpingtest.txt
+sleep 10
 
