9281f28c45acc848d6ef428e13041333570c0ca1
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-05 21:25:32 -0600

    Create novpnspeed.sh

---
+++ novpnspeed.sh
@@ -0,0 +1,41 @@
+ping us-east01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping us-central01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping us-west01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping us-south01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping us-nw01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping eu-de02.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping eu-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping eu-de01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping eu-ru01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping eu-uk01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping jp-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping ca-tor01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping au-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
+
+ping ap-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
+sleep 10s
