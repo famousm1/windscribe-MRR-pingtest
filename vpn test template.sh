43cce7ecf8bb6c7270aafd43c7b945e2dedfaddc
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-05 21:08:35 -0600

    Add files via upload

---
+++ vpn test template.sh
@@ -0,0 +1,85 @@
+windscribe connect    Bukhansan | tee -a output.txt
+
+
+windscribe connect    Hangang  | tee -a output.txt
+
+
+windscribe connect    Metro | tee -a output.txt
+
+
+windscribe connect    Victoria | tee -a output.txt
+
+
+windscribe connect    Ataturk | tee -a output.txt
+
+
+windscribe connect    Crumpets | tee -a output.txt
+
+
+windscribe connect    Custard | tee -a output.txt
+
+
+windscribe connect    Alphorn | tee -a output.txt
+
+
+windscribe connect    "No Vampires" | tee -a output.txt
+
+
+windscribe connect    Fjord     | tee -a output.txt
+
+
+windscribe connect    Canal | tee -a output.txt
+
+
+windscribe connect    Seine | tee -a output.txt
+
+
+windscribe connect    Castle | tee -a output.txt
+
+
+windscribe connect    "Expo 67" | tee -a output.txt
+
+
+windscribe connect    "Old Port" | tee -a output.txt
+
+
+windscribe connect    "Comfort Zone" | tee -a output.txt
+
+
+windscribe connect    "The 6" | tee -a output.txt
+
+
+windscribe connect    Granville | tee -a output.txt
+
+
+windscribe connect    Vansterdam            | tee -a output.txt
+
+
+windscribe connect    Mountain | tee -a output.txt
+
+
+windscribe connect    Ranch | tee -a output.txt
+
+
+windscribe connect    Cub | tee -a output.txt
+
+
+windscribe connect    Snow | tee -a output.txt
+
+
+windscribe connect    Vice | tee -a output.txt
+
+
+windscribe connect    Empire | tee -a output.txt
+
+
+windscribe connect    Precedent | tee -a output.txt
+
+
+windscribe connect    Dogg | tee -a output.txt
+
+
+windscribe connect    Cobain | tee -a output.txt
+
+
+
