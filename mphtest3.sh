10981a2c396969af230b15222f095ce4fc7c102b
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-06 05:36:13 -0600

    Update and rename mphtest2.sh to mphtest3.sh

---
+++ mphtest3.sh
@@ -0,0 +1,251 @@
+windscribe connect    Bukhansan | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Hangang  | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Metro | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Victoria | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Ataturk | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Crumpets | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Custard | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Alphorn | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    "No Vampires" | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Fjord     | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Canal | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Seine | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Castle | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    "Expo 67" | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    "Old Port" | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    "Comfort Zone" | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    "The 6" | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Granville | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Vansterdam            | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Mountain | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Ranch | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Cub | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Snow | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Vice | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Empire | tee -a mphpingtest.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
+windscribe connect    Precedent | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Dogg | tee -a mphvpnspeed.txt
+
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+windscribe connect    Cobain | tee -a mphvpnspeed.txt
+
+  ping        18.204.40.75 -c 5 | tee -a mphpingtest.txt
+  
+  ping    18.195.252.71 -c 5 | tee -a mphpingtest.txt
+  
+  ping         13.125.194.141 -c 5 | tee -a mphpingtest.txt
+
+
