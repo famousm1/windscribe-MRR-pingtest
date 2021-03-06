e727724f0517476923c5f87219026aaa1fc1a4eb
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-05 23:50:25 -0600

    Add files via upload

---
+++ vpnetherminespeed.sh
@@ -0,0 +1,308 @@
+windscribe connect    Bukhansan | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Hangang  | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Metro | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Victoria | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Ataturk | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Crumpets | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Custard | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Alphorn | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    "No Vampires" | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Fjord     | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Canal | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Seine | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Castle | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    "Expo 67" | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    "Old Port" | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    "Comfort Zone" | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    "The 6" | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Granville | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Vansterdam            | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Mountain | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Ranch | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Cub | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Snow | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Vice | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Empire | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+windscribe connect    Precedent | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Dogg | tee -a output.txt
+
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+windscribe connect    Cobain | tee -a output.txt
+sleep 10
+  ping        us1.ethermine.org:5555 -c 15
+  sleep 10
+    ping        us2.ethermine.org:5555 -c 15
+  sleep 10
+  ping    eu1.ethermine.org:5555 -c 15
+  sleep 10
+  ping         asia1.ethermine.org:5555 -c 15
+sleep 10
+
+
