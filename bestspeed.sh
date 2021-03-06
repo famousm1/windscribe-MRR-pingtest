89738d0c6b18d25a63c649085fcccc5f0e4bc7a8
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-05 21:24:04 -0600

    Create bestspeed.sh

---
+++ bestspeed.sh
@@ -0,0 +1,166 @@
+windscribe connect    Bukhansan | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Hangang  | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Metro | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Victoria | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Ataturk | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Crumpets | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Custard | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Alphorn | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    "No Vampires" | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Fjord     | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Canal | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Seine | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Castle | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    "Expo 67" | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    "Old Port" | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    "Comfort Zone" | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    "The 6" | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Granville | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Vansterdam            | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Mountain | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Ranch | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Cub | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Snow | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Vice | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Empire | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Precedent | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Dogg | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
+
+
+windscribe connect    Cobain | tee -a speedtestresults.txt
+sleep 5s
+./speedtest-go | tee -a speedtestresults.txt
+sleep 5s
