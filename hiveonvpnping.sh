695334f12f1c3663dc1cefed35cc649b7d11e0d8
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-06 01:34:00 -0600

    Update hiveonvpnping.sh

--- hiveonvpnping.sh
+++ hiveonvpnping.sh
@@ -1,307 +1,307 @@
 windscribe connect    Bukhansan | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Hangang  | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Metro | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Victoria | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Ataturk | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Crumpets | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Custard | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Alphorn | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    "No Vampires" | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Fjord     | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Canal | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Seine | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Castle | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    "Expo 67" | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    "Old Port" | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    "Comfort Zone" | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    "The 6" | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Granville | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Vansterdam            | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Mountain | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Ranch | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Cub | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Snow | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Vice | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Empire | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
 windscribe connect    Precedent | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Dogg | tee -a hiveontestpingvpn.txt.txt
 
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 windscribe connect    Cobain | tee -a hiveontestpingvpn.txt.txt
 sleep 10
-  ping        naw-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping        naw-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-    ping        aspac1-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+    ping        aspac1-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping    eu-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping    eu-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
   sleep 10
-  ping         ru-eth.hiveon.net | tee -a hiveontestpingvpn.txt.txt
+  ping         ru-eth.hiveon.net -c 15  | tee -a hiveontestpingvpn.txt.txt
 sleep 10
 
