f6826a39b1cb08b91b021af66231decca0bbd177
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-06 00:55:55 -0600

    Update vpnethermineping.sh

--- vpnethermineping.sh
+++ vpnethermineping.sh
@@ -1,307 +1,307 @@
-windscribe connect    Bukhansan | tee -a mphvpnspeed.txt
+windscribe connect    Bukhansan | tee -a ethermineping.txt
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Hangang  | tee -a mphvpnspeed.txt
+windscribe connect    Hangang  | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Metro | tee -a mphvpnspeed.txt
+windscribe connect    Metro | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Victoria | tee -a mphvpnspeed.txt
+windscribe connect    Victoria | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Ataturk | tee -a mphvpnspeed.txt
+windscribe connect    Ataturk | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Crumpets | tee -a mphvpnspeed.txt
+windscribe connect    Crumpets | tee -a ethermineping.txt
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Custard | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
 
-windscribe connect    Alphorn | tee -a mphvpnspeed.txt
-
+windscribe connect    Custard | tee -a ethermineping.txt
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    "No Vampires" | tee -a mphvpnspeed.txt
-
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    Fjord     | tee -a mphvpnspeed.txt
-
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    Canal | tee -a mphvpnspeed.txt
-
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    Seine | tee -a mphvpnspeed.txt
-
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    Castle | tee -a mphvpnspeed.txt
-
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    "Expo 67" | tee -a mphvpnspeed.txt
-
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-windscribe connect    "Old Port" | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
 
-windscribe connect    "Comfort Zone" | tee -a mphvpnspeed.txt
+windscribe connect    Alphorn | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    "The 6" | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Granville | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Vansterdam            | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Mountain | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Ranch | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Cub | tee -a mphvpnspeed.txt
+windscribe connect    "No Vampires" | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Snow | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Vice | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Empire | tee -a mphvpnspeed.txt
-sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
-  sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
-sleep 10
-
-windscribe connect    Precedent | tee -a mphvpnspeed.txt
+windscribe connect    Fjord     | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Dogg | tee -a mphvpnspeed.txt
+windscribe connect    Canal | tee -a ethermineping.txt
 
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
-windscribe connect    Cobain | tee -a mphvpnspeed.txt
+windscribe connect    Seine | tee -a ethermineping.txt
+
 sleep 10
-  ping        us1.ethermine.org | tee -a mphvpnspeed.txt
+  ping        us1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-    ping        us2.ethermine.org | tee -a mphvpnspeed.txt
+    ping        us2.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping    eu1.ethermine.org | tee -a mphvpnspeed.txt
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
   sleep 10
-  ping         asia1.ethermine.org | tee -a mphvpnspeed.txt
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    Castle | tee -a ethermineping.txt
+
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    "Expo 67" | tee -a ethermineping.txt
+
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    "Old Port" | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    "Comfort Zone" | tee -a ethermineping.txt
+
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    "The 6" | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Granville | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Vansterdam            | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Mountain | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Ranch | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Cub | tee -a ethermineping.txt
+
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    Snow | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Vice | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Empire | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+
+windscribe connect    Precedent | tee -a ethermineping.txt
+
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    Dogg | tee -a ethermineping.txt
+
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
+sleep 10
+windscribe connect    Cobain | tee -a ethermineping.txt
+sleep 10
+  ping        us1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+    ping        us2.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping    eu1.ethermine.org | tee -a ethermineping.txt
+  sleep 10
+  ping         asia1.ethermine.org | tee -a ethermineping.txt
 sleep 10
 
