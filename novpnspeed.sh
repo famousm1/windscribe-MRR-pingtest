eb787c1988b7c2b3f5d35e735f045891c8455f90
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-11 13:23:43 -0600

    Update novpnspeed.sh

--- novpnspeed.sh
+++ novpnspeed.sh
@@ -1,41 +1,75 @@
-ping us-east01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping us-east01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping us-central01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping us-central01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping us-west01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping us-west01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping us-south01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping us-south01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping us-nw01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping us-nw01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping eu-de02.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping eu-de02.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping eu-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping eu-01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping eu-de01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping eu-de01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping eu-ru01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping eu-ru01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping eu-uk01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping eu-uk01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping jp-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping jp-01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping ca-tor01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping ca-tor01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping au-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping au-01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
 
-ping ap-01.miningrigrentals.com -c 15 | tee -a stocklatency.txt 
-sleep 10s
+ping ap-01.miningrigrentals.com -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping us-east.ethash-hub.miningpoolhub.com -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping europe.ethash-hub.miningpoolhub.com -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping asia.ethash-hub.miningpoolhub.com -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping aspac1-eth.hiveon.net -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping naw-eth.hiveon.net -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping eu-eth.hiveon.net -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping ru-eth.hiveon.net -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping asia1.ethermine.org -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping eu1.ethermine.org -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping us1.ethermine.org -c 20 | tee -a stocklatency.txt 
+sleep 10
+
+ping us2.ethermine.org -c 20 | tee -a stocklatency.txt 
+sleep 10
+
