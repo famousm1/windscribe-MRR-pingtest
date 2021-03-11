7e01f2fceb20047bfb8c1fb834fba4942facfe7a
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-11 13:26:48 -0600

    Update vpn test template.sh

--- vpn test template.sh
+++ vpn test template.sh
@@ -1,85 +1,874 @@
-windscribe connect    Bukhansan | tee -a output.txt
+windscribe connect Mountain | tee -a output.txt
 
 
-windscribe connect    Hangang  | tee -a output.txt
 
 
-windscribe connect    Metro | tee -a output.txt
+windscribe connect Piedmont | tee -a output.txt
 
 
-windscribe connect    Victoria | tee -a output.txt
 
 
-windscribe connect    Ataturk | tee -a output.txt
+windscribe connect Ammo | tee -a output.txt
 
 
-windscribe connect    Crumpets | tee -a output.txt
 
 
-windscribe connect    Custard | tee -a output.txt
+windscribe connect BBQ | tee -a output.txt
 
 
-windscribe connect    Alphorn | tee -a output.txt
 
 
-windscribe connect    "No Vampires" | tee -a output.txt
+windscribe connect Ranch | tee -a output.txt
 
 
-windscribe connect    Fjord     | tee -a output.txt
 
 
-windscribe connect    Canal | tee -a output.txt
+windscribe connect Barley | tee -a output.txt
 
 
-windscribe connect    Seine | tee -a output.txt
 
 
-windscribe connect    Castle | tee -a output.txt
+windscribe connect Hops | tee -a output.txt
 
 
-windscribe connect    "Expo 67" | tee -a output.txt
 
 
-windscribe connect    "Old Port" | tee -a output.txt
+windscribe connect Glinda | tee -a output.txt
 
 
-windscribe connect    "Comfort Zone" | tee -a output.txt
 
 
-windscribe connect    "The 6" | tee -a output.txt
+windscribe connect MIT | tee -a output.txt
 
 
-windscribe connect    Granville | tee -a output.txt
 
 
-windscribe connect    Vansterdam            | tee -a output.txt
+windscribe connect Bill | tee -a output.txt
 
 
-windscribe connect    Mountain | tee -a output.txt
 
 
-windscribe connect    Ranch | tee -a output.txt
+windscribe connect Earnhardt | tee -a output.txt
 
 
-windscribe connect    Cub | tee -a output.txt
 
 
-windscribe connect    Snow | tee -a output.txt
+windscribe connect Cub | tee -a output.txt
 
 
-windscribe connect    Vice | tee -a output.txt
 
 
-windscribe connect    Empire | tee -a output.txt
+windscribe connect "The L" | tee -a output.txt
 
 
-windscribe connect    Precedent | tee -a output.txt
 
 
-windscribe connect    Dogg | tee -a output.txt
+windscribe connect Wrigley | tee -a output.txt
 
 
-windscribe connect    Cobain | tee -a output.txt
+
+
+windscribe connect Brown | tee -a output.txt
+
+
+
+
+windscribe connect "Coney Dog" | tee -a output.txt
+
+
+
+
+windscribe connect "Florida Man" | tee -a output.txt
+
+
+
+
+windscribe connect Snow | tee -a output.txt
+
+
+
+
+windscribe connect Vice | tee -a output.txt
+
+
+
+
+windscribe connect Situation | tee -a output.txt
+
+
+
+
+windscribe connect Empire | tee -a output.txt
+
+
+
+
+windscribe connect Gotham | tee -a output.txt
+
+
+
+
+windscribe connect Insomnia | tee -a output.txt
+
+
+
+
+windscribe connect "Tofu Driver" | tee -a output.txt
+
+
+
+
+windscribe connect Cheese | tee -a output.txt
+
+
+
+
+windscribe connect "Fresh Prince" | tee -a output.txt
+
+
+
+
+windscribe connect "Cuban Sandwich" | tee -a output.txt
+
+
+
+
+windscribe connect Precedent | tee -a output.txt
+
+
+
+
+windscribe connect "Oregon Trail" | tee -a output.txt
+
+
+
+
+windscribe connect Casino | tee -a output.txt
+
+
+
+
+windscribe connect Cube | tee -a output.txt
+
+
+
+
+windscribe connect Dogg | tee -a output.txt
+
+
+
+
+windscribe connect Eazy | tee -a output.txt
+
+
+
+
+windscribe connect Lamar | tee -a output.txt
+
+
+
+
+windscribe connect Pac | tee -a output.txt
+
+
+
+
+windscribe connect Floatie | tee -a output.txt
+
+
+
+
+windscribe connect Sanitation | tee -a output.txt
+
+
+
+
+windscribe connect Santana | tee -a output.txt
+
+
+
+
+windscribe connect Inside | tee -a output.txt
+
+
+
+
+windscribe connect Cobain | tee -a output.txt
+
+
+
+
+windscribe connect Cornell | tee -a output.txt
+
+
+
+
+windscribe connect Hendrix | tee -a output.txt
+
+
+
+
+windscribe connect Radiohall | tee -a output.txt
+
+
+
+
+windscribe connect Howe | tee -a output.txt
+
+
+
+
+windscribe connect "Bagel Poutine" | tee -a output.txt
+
+
+
+
+windscribe connect "Expo 67" | tee -a output.txt
+
+
+
+
+windscribe connect "Old Port" | tee -a output.txt
+
+
+
+
+windscribe connect "Comfort Zone" | tee -a output.txt
+
+
+
+
+windscribe connect "The 6" | tee -a output.txt
+
+
+
+
+windscribe connect Granville | tee -a output.txt
+
+
+
+
+windscribe connect Stanley | tee -a output.txt
+
+
+
+
+windscribe connect Vansterdam | tee -a output.txt
+
+
+
+
+windscribe connect Mansbridge | tee -a output.txt
+
+
+
+
+windscribe connect Boltzmann | tee -a output.txt
+
+
+
+
+windscribe connect Hofburg | tee -a output.txt
+
+
+
+
+windscribe connect Guildhouse | tee -a output.txt
+
+
+
+
+windscribe connect Nevski | tee -a output.txt
+
+
+
+
+windscribe connect Tkalciceva | tee -a output.txt
+
+
+
+
+windscribe connect "Blue Lagoon" | tee -a output.txt
+
+
+
+
+windscribe connect Vltava | tee -a output.txt
+
+
+
+
+windscribe connect Staromak | tee -a output.txt
+
+
+
+
+windscribe connect Rosenborg | tee -a output.txt
+
+
+
+
+windscribe connect Lennujaam | tee -a output.txt
+
+
+
+
+windscribe connect Sauna | tee -a output.txt
+
+
+
+
+windscribe connect Tram | tee -a output.txt
+
+
+
+
+windscribe connect Jardin | tee -a output.txt
+
+
+
+
+windscribe connect Seine | tee -a output.txt
+
+
+
+
+windscribe connect Castle | tee -a output.txt
+
+
+
+
+windscribe connect Wiener | tee -a output.txt
+
+
+
+
+windscribe connect Agora | tee -a output.txt
+
+
+
+
+windscribe connect Odeon | tee -a output.txt
+
+
+
+
+windscribe connect Parthenon | tee -a output.txt
+
+
+
+
+windscribe connect Danube | tee -a output.txt
+
+
+
+
+windscribe connect "Fuzzy Pony" | tee -a output.txt
+
+
+
+
+windscribe connect Reyka | tee -a output.txt
+
+
+
+
+windscribe connect Dullahan | tee -a output.txt
+
+
+
+
+windscribe connect Guinness | tee -a output.txt
+
+
+
+
+windscribe connect "Yam Park" | tee -a output.txt
+
+
+
+
+windscribe connect Zion | tee -a output.txt
+
+
+
+
+windscribe connect Duomo | tee -a output.txt
+
+
+
+
+windscribe connect Galleria | tee -a output.txt
+
+
+
+
+windscribe connect Colosseum | tee -a output.txt
+
+
+
+
+windscribe connect Daugava | tee -a output.txt
+
+
+
+
+windscribe connect Vecriga | tee -a output.txt
+
+
+
+
+windscribe connect Talksa | tee -a output.txt
+
+
+
+
+windscribe connect Dendrarium | tee -a output.txt
+
+
+
+
+windscribe connect Bicycle | tee -a output.txt
+
+
+
+
+windscribe connect Canal | tee -a output.txt
+
+
+
+
+windscribe connect "Red Light" | tee -a output.txt
+
+
+
+
+windscribe connect Tulip | tee -a output.txt
+
+
+
+
+windscribe connect Vardar | tee -a output.txt
+
+
+
+
+windscribe connect Fjord | tee -a output.txt
+
+
+
+
+windscribe connect Chopin | tee -a output.txt
+
+
+
+
+windscribe connect Curie | tee -a output.txt
+
+
+
+
+windscribe connect Vistula | tee -a output.txt
+
+
+
+
+windscribe connect Bairro | tee -a output.txt
+
+
+
+
+windscribe connect "No Vampires" | tee -a output.txt
+
+
+
+
+windscribe connect "Devin Castle" | tee -a output.txt
+
+
+
+
+windscribe connect Batllo | tee -a output.txt
+
+
+
+
+windscribe connect Prado | tee -a output.txt
+
+
+
+
+windscribe connect Djurgarden | tee -a output.txt
+
+
+
+
+windscribe connect Ikea | tee -a output.txt
+
+
+
+
+windscribe connect Syndrome | tee -a output.txt
+
+
+
+
+windscribe connect Alphorn | tee -a output.txt
+
+
+
+
+windscribe connect Altstadt | tee -a output.txt
+
+
+
+
+windscribe connect Lindenhof | tee -a output.txt
+
+
+
+
+windscribe connect Medina | tee -a output.txt
+
+
+
+
+windscribe connect Keeper Willie | tee -a output.txt
+
+
+
+
+windscribe connect Biscuits | tee -a output.txt
+
+
+
+
+windscribe connect Crumpets | tee -a output.txt
+
+
+
+
+windscribe connect Custard | tee -a output.txt
+
+
+
+
+windscribe connect United | tee -a output.txt
+
+
+
+
+windscribe connect "The Tube" | tee -a output.txt
+
+
+
+
+windscribe connect Besa | tee -a output.txt
+
+
+
+
+windscribe connect Caspian | tee -a output.txt
+
+
+
+
+windscribe connect Burek | tee -a output.txt
+
+
+
+
+windscribe connect Adyar | tee -a output.txt
+
+
+
+
+windscribe connect Mahim | tee -a output.txt
+
+
+
+
+windscribe connect Mutha | tee -a output.txt
+
+
+
+
+windscribe connect "Sleepy Lenin" | tee -a output.txt
+
+
+
+
+windscribe connect Hermitage | tee -a output.txt
+
+
+
+
+windscribe connect Shnur | tee -a output.txt
+
+
+
+
+windscribe connect Rakia | tee -a output.txt
+
+
+
+
+windscribe connect District | tee -a output.txt
+
+
+
+
+windscribe connect Ellis Park | tee -a output.txt
+
+
+
+
+windscribe connect Lindfield | tee -a output.txt
+
+
+
+
+windscribe connect Teleferik | tee -a output.txt
+
+
+
+
+windscribe connect Ataturk | tee -a output.txt
+
+
+
+
+windscribe connect Galata | tee -a output.txt
+
+
+
+
+windscribe connect Ottoman | tee -a output.txt
+
+
+
+
+windscribe connect Borscht | tee -a output.txt
+
+
+
+
+windscribe connect Lofty | tee -a output.txt
+
+
+
+
+windscribe connect Oval | tee -a output.txt
+
+
+
+
+windscribe connect "Bad Koala" | tee -a output.txt
+
+
+
+
+windscribe connect "Good Koala" | tee -a output.txt
+
+
+
+
+windscribe connect Burley | tee -a output.txt
+
+
+
+
+windscribe connect "Port Phillip" | tee -a output.txt
+
+
+
+
+windscribe connect Yarra | tee -a output.txt
+
+
+
+
+windscribe connect Herdsman | tee -a output.txt
+
+
+
+
+windscribe connect "Kings Park" | tee -a output.txt
+
+
+
+
+windscribe connect "Opera House" | tee -a output.txt
+
+
+
+
+windscribe connect Squidney | tee -a output.txt
+
+
+
+
+windscribe connect Hauraki | tee -a output.txt
+
+
+
+
+windscribe connect Parnell | tee -a output.txt
+
+
+
+
+windscribe connect Phooey | tee -a output.txt
+
+
+
+
+windscribe connect Victoria | tee -a output.txt
+
+
+
+
+windscribe connect Menteng | tee -a output.txt
+
+
+
+
+windscribe connect Bosozoku | tee -a output.txt
+
+
+
+
+windscribe connect Drift | tee -a output.txt
+
+
+
+
+windscribe connect Perdana | tee -a output.txt
+
+
+
+
+windscribe connect Pasig | tee -a output.txt
+
+
+
+
+windscribe connect Zambales | tee -a output.txt
+
+
+
+
+windscribe connect Garden | tee -a output.txt
+
+
+
+
+windscribe connect "Marina Bay" | tee -a output.txt
+
+
+
+
+windscribe connect SMRT | tee -a output.txt
+
+
+
+
+windscribe connect Bukhansan | tee -a output.txt
+
+
+
+
+windscribe connect Hangang | tee -a output.txt
+
+
+
+
+windscribe connect Metro | tee -a output.txt
+
+
+
+
+windscribe connect Datong | tee -a output.txt
+
+
+
+
+windscribe connect Hangover | tee -a output.txt
+
+
+
+
+windscribe connect "Khao San" | tee -a output.txt
+
+
+
+
+windscribe connect Khalifa | tee -a output.txt
+
+
+
+
+windscribe connect "Red River" | tee -a output.txt
+
+
+
+
+windscribe connect Kaiju | tee -a output.txt
+
+
+
+
+windscribe connect Tango | tee -a output.txt
+
+
+
+
+windscribe connect Mercadao | tee -a output.txt
+
+
+
+
+windscribe connect Pinacoteca | tee -a output.txt
+
+
+
+
+windscribe connect Cueca | tee -a output.txt
+
+
+
+
+windscribe connect Rololandia | tee -a output.txt
+
+
+
+
+windscribe connect "White Coffee" | tee -a output.txt
+
+
+
+
+windscribe connect Cabanas | tee -a output.txt
+
+
+
+
+windscribe connect Cojones | tee -a output.txt
+
+
+
+
+windscribe connect Papers | tee -a output.txt
+
+
+
+
+windscribe connect Amaru | tee -a output.txt
+
+
+
+
+windscribe connect Station | tee -a output.txt
 
 
 
