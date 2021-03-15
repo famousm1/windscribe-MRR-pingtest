46ab4119bc9a61bf63877e1e93168b5f1aa9d234
famousm1 <75544516+famousm1@users.noreply.github.com>
2021-03-15 13:23:33 -0500

    Add files via upload

---
+++ undetectedspeedtest.sh
@@ -0,0 +1,145 @@
+FILE=./speedtest-go_1.1.1_Linux_x86_64.tar.gz
+if [ ! -f "$FILE" ]; then
+    wget https://github.com/showwin/speedtest-go/releases/download/v1.1.1/speedtest-go_1.1.1_Linux_x86_64.tar.gz
+tar -xvf speedtest-go_1.1.1_Linux_x86_64.tar.gz
+fi
+
+windscribe connect Glinda | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Brown | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect "Coney Dog" | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Howe | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Stanley | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect "Blue Lagoon" | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Agora | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Odeon | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Reyka | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Zion | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Vecriga | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Vardar | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Medina | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Mutha | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect "Sleepy Lenin"  | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Ottoman | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Menteng | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Zambales | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Bukhansan | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Metro | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Hangover | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect "Red River" | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Tango | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Mercadao | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Rololandia | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Cojones | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Papers | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
+
+windscribe connect Amaru | tee -a UndetectedVpnSpeeds.txt
+sleep 2
+./speedtest-go | tee -a UndetectedVpnSpeeds.txt
+sleep 10
